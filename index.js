08be43f052dd2d8e8e2693651d60779383f1de68
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-08-04 19:40:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var Algorithmia = require("algorithmia");
+const Algorithmia = require("algorithmia");
 const data = require('./sentimentos.json')
 
-var rest;
 
-function response(object){
+function ListFeelings(object){
     Algorithmia.client("simaWHJ74dK0KqN5pk1+vIF51tS1")
     .algo("nlp/SentimentAnalysis/1.0.5?timeout=300") // timeout is optional
     .pipe(object)
     .then(function(response) {
-        rest = response.get();
-        for(let i = 0; i < rest.length; i++){
-            if(rest[i].sentiment < 0){
-                console.log(`Você provavelmente está triste; Sentimento:${rest[i].sentiment}::`);
-                console.log(`Frase: ${rest[i].document}. \n`);
+        var response = response.get();
+        response.map(result => {
+            const sad = result.sentiment < 0
+            if (sad){
+                console.log(`Você provavelmente está triste; Sentimento:${result.sentiment}::`);
+                console.log(`Frase: ${result.document}. \n`);
                 console.log('------------------------------------------------------- \n');
             }
-            if(rest[i].sentiment > 0){
-                console.log(`Você provavelmente está feliz; Sentimento: ${rest[i].sentiment}::`);
-                console.log(`Frase: ${rest[i].document}. \n`);
+            else {
+                console.log(`Você provavelmente está Feliz; Sentimento:${result.sentiment}::`);
+                console.log(`Frase: ${result.document}. \n`);
                 console.log('------------------------------------------------------- \n');
             }
-        }
+        })
+        
     });
 }
 
-response(data)
+ListFeelings(data)
